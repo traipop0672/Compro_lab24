4d62178a33a277ac8acb427527d20b5873dc3549
Traipop <traipopwichiansarn@gmail.com>
2026-03-10 18:09:51 +0700

    lab24_3 completed

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 //Write function count() here.
 
+int count(int arr[], int size) {
+    set<int> uniqueNumbers;
+    
+    for (int i = 0; i < size; i++) {
+        uniqueNumbers.insert(arr[i]); 
+    }
+    
+    return uniqueNumbers.size();
+}
+
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
 	
